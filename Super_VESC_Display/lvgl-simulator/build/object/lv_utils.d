ae8963262de865d02805f7a2512b940cdf16ee06
aleksei <payalne@gmail.com>
2025-10-02 14:39:26 +0200

    dashboard

--- Super_VESC_Display/lvgl-simulator/build/object/lv_utils.d
+++
@@ -1,5 +0,0 @@
-build/object/lv_utils.o: \
- c:\users\payal\work\super_vesc_display\super_vesc_display\lvgl\src\misc\lv_utils.c \
- c:\users\payal\work\super_vesc_display\super_vesc_display\lvgl\src\misc\lv_utils.h
-
-c:\users\payal\work\super_vesc_display\super_vesc_display\lvgl\src\misc\lv_utils.h:
--- Super_VESC_Display/lvgl-simulator/build/object/qrcodegen.d
+++
@@ -1,5 +0,0 @@
-build/object/qrcodegen.o: \
- c:\users\payal\work\super_vesc_display\super_vesc_display\lvgl\src\extra\libs\qrcode\qrcodegen.c \
- c:\users\payal\work\super_vesc_display\super_vesc_display\lvgl\src\extra\libs\qrcode\qrcodegen.h
-
-c:\users\payal\work\super_vesc_display\super_vesc_display\lvgl\src\extra\libs\qrcode\qrcodegen.h:
